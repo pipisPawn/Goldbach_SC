401227fd1381e1d2ac4f8d3dfe56bf8ff0510c7f
cubonnn <yingzhenglai@yahoo.ca>
2021-06-07 11:52:48 -0400

    user manager moduler

---
+++ shop-user-manage/src/main/java/com/goldbach/shop/user/ShopUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.goldbach.shop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopUserManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopUserManageApplication.class, args);
+	}
+
+}
